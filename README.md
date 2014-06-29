d33fbdbfb1d5dfc2785eec192a6a3da0fcc750fb
Layton McDaniel <laytonmcdaniel@gmail.com>
2014-06-28 21:12:00 -0400

    Added fabfile and README

---
+++ README.md
@@ -0,0 +1,3 @@
+Basic Flask server used to interact with the GPIO pins on a Raspberry Pi.
+The code from the following URL was used as a starting point:
+http://mattrichardson.com/Raspberry-Pi-Flask/
---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import env, task, local, run, sudo, hide
+
+env.root_dir = env.real_fabfile.replace('fabfile.py', '')
+
+@task(alias='copy')
+def copy_repo():
+	with hide('output', 'running'):
+		hostname = str(local('hostname', capture=True))
+
+	if hostname == 'TheDude':
+		destination = 'pi:~/flask/'
+	elif hostname == 'pibox':
+		destination = 'TheDude:~/pi/flask/'
+	else:
+		print "unkown host '{hostname}'.".format(hostname=hostname)
+		return
+
+	with hide('running'):
+		local('scp -r {root_dir}* {dest}'.format(root_dir=env.root_dir, dest=destination))
+
+
+@task
+def show_root_dir():
+	print env.root_dir
