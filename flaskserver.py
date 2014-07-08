d889d7a524ea87706b169c7190d80668fa654e67
Layton McDaniel <laytonmcdaniel@gmail.com>
2014-07-08 18:35:34 -0400

    Camera stream and ajax

--- flaskserver.py
+++ flaskserver.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, render_template, jsonify, request
 import datetime
 import RPi.GPIO as GPIO
 app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
-# GPIO.cleanup()
+## GPIO.cleanup()
 GPIO.setwarnings(False)
 
 pins = { 17 : {'name': 'led01', 'state': GPIO.LOW} }
+# pins = {}
 
 for pin in pins:
 	GPIO.setup(pin,GPIO.OUT)
@@ -18,12 +19,28 @@ def main():
    # For each pin, read the pin state and store it in the pins dictionary:
    for pin in pins:
       pins[pin]['state'] = GPIO.input(pin)
+
    # Put the pin dictionary into the template data dictionary:
    templateData = {
       'pins' : pins
       }
    # Pass the template data into the template main.html and return it to the user
    return render_template('main.html', **templateData)
+
+@app.route('/get_content')
+def get_content():
+   buttonID = request.args.get('id', 'Button ID not found.')
+
+   if buttonID == 'button1':
+      content = 'Button 1 content area'
+   elif buttonID == 'button2':
+      content = 'You clicked on button 2.  Good for you!'
+   elif buttonID == 'button3':
+      content = "This is the content area for button 3.  Isn't it spiffy?"
+   else:
+      content = buttonID
+
+   return jsonify(result=content)
 
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
@@ -54,11 +71,11 @@ def action(changePin, action):
    # Along with the pin dictionary, put the message into the template data dictionary:
    templateData = {
       'message' : message,
-      'pins' : pins
+      'pins' : pins,
    }
 
    return render_template('main.html', **templateData)
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+   app.run(host='0.0.0.0', port=8088, debug=True)
